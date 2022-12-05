d089ee7b9abd94ccc3c9162773c31e89ffbdcc67
fbgabriana <fbgabriana@gmail.com>
2022-12-06 02:35:59 +0800

    Random updates

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 const dbconfig = require("./db.config.js");
 
-const sql = mysql.createConnection({
+const sql = mysql.createPool({
 	user: dbconfig.user,
 	host: dbconfig.host,
 	password: dbconfig.password,
