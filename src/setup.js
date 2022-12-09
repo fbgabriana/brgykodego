cbf2365f109c84d2b56d44c8a40ffdb1725b762d
fbgabriana <fbgabriana@gmail.com>
2022-12-09 12:00:32 +0800

    Random updates

--- src/setup.js
+++ src/setup.js
@@ -1,10 +1,15 @@
-const exec = require('child_process').exec;
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const dbconfig = require("./db.config.js");
 
 const stdout = (process.platform == "win32") ? "type" : "cat";
 
 const setup = async () => {
 	console.log("\nCreating database:");
-	await exec(`${stdout} src/sql/create-database.sql src/sql/create-tables.sql src/sql/populate-tables.sql | mysql -u root`);
+	if (! process.env.DATABASE_URL ) {
+		await exec(`${stdout} src/sql/create-database.sql | mysql -u root`);
+	}
+	await exec(`${stdout} src/sql/create-tables.sql src/sql/populate-tables.sql | mysql --user=${dbconfig.user} --password=${dbconfig.password} --host=${dbconfig.host} --database=${dbconfig.database}`);
 	require("./pages.js");
 }
 
