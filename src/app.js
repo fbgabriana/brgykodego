e09f3eab98cba4a5d31ae4b44e62bbd11a508808
fbgabriana <fbgabriana@gmail.com>
2023-01-06 21:59:27 +0800

    Random updates

--- src/app.js
+++ src/app.js
@@ -356,6 +356,7 @@ fs.readFile(`${publicpath}/template.html`, "utf8").then(content => {
 						switch (req.method) {
 						case "GET":
 							res.writeHead(200, {"Content-Type": "application/json"});
+							console.log(referer.origin, app.homepage)
 							if (referer.origin === app.homepage) {
 								await sql.query(`SELECT
 									hsl_id,
